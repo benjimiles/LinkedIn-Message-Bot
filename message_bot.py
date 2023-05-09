f42a280d804081f1b63f27f400ece7cb9e479450
Benji Miles <benjimiles1999@gmail.com>
2023-05-09 15:30:59 +0200

    Added the Imput fields for Username & Password

--- message_bot.py
+++ message_bot.py
@@ -7,3 +7,6 @@ import time
 
 driver = webdriver.Chrome("Path_to_chrome_driver")
 driver.get("https://www.linkedin.com")
+
+username = driver.find_element(by=By.NAME, value="session_key")
+password = driver.find_element(by=By.NAME, value="session_password")
